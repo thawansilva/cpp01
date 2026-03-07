3e0015375afe531281657e187b1255c8956aa9b6
Thawan Silva <twnpsilva@gmail.com>
2026-03-07 10:15:07 -0300

    feat: finish ex05

--- ex05/Harl.hpp
+++ ex05/Harl.hpp
@@ -1,6 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thaperei <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/07 07:54:32 by thaperei          #+#    #+#             */
+/*   Updated: 2026/03/07 10:11:31 by thaperei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef HARL_CPP
 #define HARL_CPP
 #include <iostream>
+#define NUM_LEVELS 4
 
 class Harl
 {
@@ -15,4 +28,6 @@ private:
 	void warning( void );
 	void error( void );
 };
+
+typedef void (Harl::*functionPerLevel)(void);
 #endif
