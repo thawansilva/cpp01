5ace3762231adb851383765913e340a035c1f123
Thawan Silva <twnpsilva@gmail.com>
2026-03-07 11:48:17 -0300

    fix: add header, and fix print address of pointer

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thaperei <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/07 11:45:43 by thaperei          #+#    #+#             */
+/*   Updated: 2026/03/07 11:46:52 by thaperei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <iostream>
 
 int main()
@@ -8,7 +20,7 @@ int main()
 
 	// Addresses
 	std::cout << &str << std::endl;
-	std::cout << &(*stringPTR) << std::endl;
+	std::cout << stringPTR << std::endl;
 	std::cout << &stringREF << std::endl;
 
 	// Values
