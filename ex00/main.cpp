414a145df6f670915b98e16a36aac85371b8e641
Thawan Silva <twnpsilva@gmail.com>
2026-03-07 11:45:32 -0300

    fix: Add header

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,9 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thaperei <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/07 11:44:18 by thaperei          #+#    #+#             */
+/*   Updated: 2026/03/07 11:44:20 by thaperei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "Zombie.hpp"
 
 int main()
 {
-	randomChump("Ada");
-	Zombie* thawan = newZombie("Thawan");
+	randomChump("STACK");
+	Zombie* thawan = newZombie("HEAP");
 	thawan->announce();
 	delete thawan;
 	return 0;
